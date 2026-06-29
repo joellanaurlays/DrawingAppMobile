41b2fc4d68171dc73128313ec925272c3f1a48cc
Joella <joellanaurlays@gmail.com>
2026-06-29 20:12:53 +0300

    update, install dotenv + change the config for firebase

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const server = require('http').createServer();
 const io = require('socket.io')(server, {
   cors: { origin: "*", methods: ["GET", "POST"] }
@@ -5,8 +6,17 @@ const io = require('socket.io')(server, {
 
 // INITIALISATION FIREBASE ADMIN
 const admin = require('firebase-admin');
-const serviceAccount = require("./config/serviceAccountKey.json"); 
-
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
