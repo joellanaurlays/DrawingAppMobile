83b2cf3df530618f1796a2faddebd407e4aa71b5
Joella <joellanaurlays@gmail.com>
2026-06-29 19:37:06 +0300

    deploy application

--- backend/src/server.js
+++ backend/src/server.js
@@ -85,4 +85,5 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => console.log('Serveur persistant actif sur le port 3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Serveur persistant actif sur le port ${PORT}`));
